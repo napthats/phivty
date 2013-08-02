31e0e7d186cb52401baeb83b9854132e8266efcc
Naoki Yoshinaga <nap525@gmail.com>
2013-08-02 23:32:29 +0900

    Use m57 protocol instead of old map protocol.

--- PhiVty/Protocol.hs
+++ PhiVty/Protocol.hs
@@ -12,18 +12,15 @@ data ServerProtocol =
 parse :: String -> ServerProtocol
 parse ('#':protocol) =
   case takeWhile ((/=) ' ') protocol of
-    "map" ->
+    "m57" ->
       if protocol !! 4 == 'M'
         then
           let (_, map_str) = splitAt 17 protocol in
-          Map $ "???????" ++ (foldl
+          Map $ (foldl
             (\acc ord ->
-              (if ord `mod` 10 == 0 then "?" else "")
-              ++ [map_str !! ord]
-              ++ (if ord `mod` 10 == 8 then "?" else "")
-              ++ acc)
-            "???????"
-            [48, 46..0])
+              (map_str !! ord) : acc)
+            ""
+            [96, 94..0])
         else Unknown
     _ -> Unknown
 parse mes =
